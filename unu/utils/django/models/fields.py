5c4203dd7cf8a2fe4a634fffb8c7b34fcc483073
Danijel Pančić <danijel.pancic@unuaondo.com>
2021-08-02 12:52:33 +0200

    image field helper UploadToPathAndRename

--- unu/utils/django/models/fields.py
+++ unu/utils/django/models/fields.py
@@ -1,8 +1,12 @@
 import decimal
 import logging
+import os
 
 
 from django.db import models
+from django.utils.deconstruct import deconstructible
+from django.utils import text
+from django.conf import settings
 
 
 logger = logging.getLogger(__name__)
@@ -22,3 +26,27 @@ class PriceField(models.DecimalField):
 		kwargs['decimal_places'] = 8
 		kwargs['default'] = decimal.Decimal('0.0')
 		super(PriceField, self).__init__(*args, **kwargs)
+
+
+@deconstructible
+class UploadToPathAndRename(object):
+	def __init__(self, path, suffix=None, density=None):
+		self.sub_path = path
+		if suffix is None or suffix.strip() == '':
+			self.suffix = ''
+		else:
+			self.suffix = f'-{suffix}'
+		if density is None or density.strip() == '':
+			self.density = ''
+		else:
+			self.density = f'-{density}'
+
+	def __call__(self, instance, filename):
+		try:
+			filename = f'{text.slugify(instance.get_full_name())}{self.suffix}{self.density}.png'
+		except Exception as e:
+			logger.exception(f'Could not make a pretty name for image {filename} on class instance {type(instance).__name__} with id {instance.pk}')
+		full_path = os.path.join(settings.MEDIA_ROOT, self.sub_path, filename)
+		if os.path.exists(full_path):
+			os.remove(full_path)
+		return os.path.join(self.sub_path, filename)
